d2348cb22d69957cd567ef7bbe4320f27923b993
yashPundhir <yash.pundhir.prof@gmail.com>
2023-10-05 23:59:39 +0530

    todoSlice created for basic todo functionalities

---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,19 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+	todos: [
+		{
+			id: 1,
+			text: "hello world",
+		},
+	],
+};
+
+export const todoSlice = createSlice({
+	name: "todo",
+	initialState,
+	reducres: {
+		addTodo: () => {},
+		removeTodo: () => {},
+	},
+});
