33ba7f9f21dea7c5df2c21537b2dc911e65bfae8
yashPundhir <yash.pundhir.prof@gmail.com>
2023-10-23 23:55:43 +0530

    Form is now functional, todos can be added

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
 
+import { useDispatch } from "react-redux";
+
+import { addTodo } from "../features/todo/todoSlice";
+
 const AddTodo = () => {
 	const [input, setInput] = useState("");
 
+	const dispatch = useDispatch();
+
 	const addTodoHandler = (e) => {
 		e.preventDefault();
+
+		dispatch(addTodo(input));
+
+		setInput("");
 	};
 
 	return (
