428175778af726050c107989287ecf5a71c0141f
yashPundhir <yash.pundhir.prof@gmail.com>
2023-10-23 23:58:54 +0530

    Todos component made to display all Todos

---
+++ src/components/Todos.jsx
@@ -0,0 +1,9 @@
+// import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeTodo } from "../features/todo/todoSlice";
+
+const Todos = () => {
+	return <div>Todos</div>;
+};
+
+export default Todos;
