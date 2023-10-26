9d8ad0d138c85b9fb0d75d1ab9838245ad7c099c
yashPundhir <yash.pundhir.prof@gmail.com>
2023-10-26 23:59:10 +0530

    Todos list added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 				Redux Toolkit Practice Project
 			</h1>
 			<AddTodo />
+			<Todos />
 		</div>
 	);
 }
